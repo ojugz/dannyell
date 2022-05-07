f2343428f220158b9cb88504acd00dac0dfee122
ojugz <mcdanielsojugo@gmail.com>
2022-05-07 11:09:00 +0100

    Improving on the view

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -21,7 +21,7 @@ export default function Topbar() {
           <div className="topbarIconContainer">
             <Settings />
           </div>
-          <img src="https://images.pexels.com/photos/1526814/pexels-photo-1526814.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" className="topAvatar" />
+          <img src="https://scontent.flos8-1.fna.fbcdn.net/v/t1.6435-9/120271271_2848414075389262_8545576178151574951_n.jpg?_nc_cat=105&ccb=1-6&_nc_sid=730e14&_nc_eui2=AeFT_hqJhVCgOgJFF_SPYJJCD_yOFzi8uIsP_I4XOLy4i128N8nVbXBxgDhS7324U0gLSQs3BZxYqmfDXMYm9UK8&_nc_ohc=GJJdiVqjB7MAX8S9rQ6&_nc_ht=scontent.flos8-1.fna&oh=00_AT8BmMJ-Ghs_Natfem3uLxqJ2fcrPesIy01HsRVNIRlLmg&oe=629C0B0E" alt="" className="topAvatar" />
         </div>
       </div>
     </div>
